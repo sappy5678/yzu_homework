4c4f732c12f247f06ff3cee70b8a773a1adaed72
sappy5678 <sappy5678@gmail.com>
2017-05-24 22:41:25 +0800

    Create hw_1_4.cpp

---
+++ Freshman/First_Semester/C_Plus_Plus_1/homework01/hw_1_4.cpp
@@ -0,0 +1,34 @@
+#include <iostream> // allows program to perform input and output
+using namespace std; // program uses names from the std namespace
+
+int main()
+{
+    int number; // integer to square and cube
+	
+	//print 
+	cout << "integer square  cube\n";
+	number = 0;
+	cout << number << "\t" << number*number << "\t" << number*number*number << endl;
+	number = 1;
+	cout << number << "\t" << number*number << "\t" << number*number*number << endl;
+	number = 2;
+	cout << number << "\t" << number*number << "\t" << number*number*number << endl;
+	number = 3;
+	cout << number << "\t" << number*number << "\t" << number*number*number << endl;
+	number = 4;
+	cout << number << "\t" << number*number << "\t" << number*number*number << endl;
+	number = 5;
+	cout << number << "\t" << number*number << "\t" << number*number*number << endl;
+	number = 6;
+	cout << number << "\t" << number*number << "\t" << number*number*number << endl;
+	number = 7;
+	cout << number << "\t" << number*number << "\t" << number*number*number << endl;
+	number = 8;
+	cout << number << "\t" << number*number << "\t" << number*number*number << endl;
+	number = 9;
+	cout << number << "\t" << number*number << "\t" << number*number*number << endl;
+	number = 10;
+	cout << number << "\t" << number*number << "\t" << number*number*number << endl;
+
+   system( "pause" );
+} // end main
